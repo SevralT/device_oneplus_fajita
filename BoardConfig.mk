0ec6bc72abfaa6197f375109ea38b52ae17a723c
chandu078 <chandudyavanapelli03@gmail.com>
2021-05-25 16:07:40 +0300

    fajita: BoardConfig: Add Assert device

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,9 @@
 
 DEVICE_PATH := device/oneplus/fajita
 
+# Assert
+TARGET_OTA_ASSERT_DEVICE := OnePlus6T,fajita
+
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
