081df8c4e10c82375f77a8d22d3ad578e53447ee
SevralT <alex_chycherin@outlook.com>
2021-06-02 21:25:13 +0300

    fajita: OctaviOS Bringup!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_fajita.mk
+    $(LOCAL_DIR)/octavi_fajita.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_fajita-user \
-    lineage_fajita-userdebug \
-    lineage_fajita-eng
+    octavi_fajita-user \
+    octavi_fajita-userdebug \
+    octavi_fajita-eng
